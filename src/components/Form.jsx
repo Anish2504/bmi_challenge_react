30363266d7585f97f28790e933a98d9695404665
Anish2504 <anish.kanswal@gmail.com>
2020-02-24 11:55:57 +0100

    able to store information in a form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -8,20 +8,23 @@ const Form = props => {
         type="number"
         required
         placeholder="Weight in Kgs"
+        value={props.weight}
         name="weight"
         id="weight"
+        onChange={props.onChangeHandler}
       />
       <label htmlFor="height">Height</label>
       <input
         type="number"
         required
         placeholder="Height in cm"
+        value={props.height}
         name="height"
         id="height"
+        onChange={props.onChangeHandler}
       />
-      <button>Calculate BMI</button>
+      <button id='calculate'>Calculate BMI</button>
     </form>
   );
 };
  export default Form;
- 
